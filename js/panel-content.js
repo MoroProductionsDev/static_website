ef94412bb190d85de966360e72ccc2ca11c616d6
Raul Rivero Rubio <morosoft_dev@yahoo.com>
2023-05-09 21:36:01 -0700

    Implemented all the main components needed such as the accordion, panel tabs ect. Implementing the home page layout.

--- js/panel-content.js
+++ js/panel-content.js
@@ -3,21 +3,19 @@ function displayArticle(rdbutton) {
     const article_id = ["0_to_4_weeks_article", "4_weeks_to_yr_article", "yr_to_seven_yrs_article", "seven_yrs_and_more_article"];
     const articles = document.querySelectorAll('div.panel > article');
 
-    // if (rdbutton.checked) {
 
-        let activeIdx = 0;
-        rdbtn_ids.forEach((btn_id, idx) => {
-            if (rdbutton.id == btn_id) {
-                activeIdx =  idx;
-            }
-        });
+    let activeIdx = 0;
+    rdbtn_ids.forEach((btn_id, idx) => {
+        if (rdbutton.id == btn_id) {
+            activeIdx =  idx;
+        }
+    });
 
-        articles.forEach((article, idx) => {
-            if(idx == activeIdx) {
-                article.style.display = "block";
-            } else {
-                article.style.display = "none";
-            }
-        });
-    // }
+    articles.forEach((article, idx) => {
+        if(idx == activeIdx) {
+            article.style.display = "block";
+        } else {
+            article.style.display = "none";
+        }
+    });
 }
