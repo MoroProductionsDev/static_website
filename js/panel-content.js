3fb1be5b0df85c3f4a3da84c053e1b39618992b1
Raul Rivero Rubio <morosoft_dev@yahoo.com>
2023-05-12 04:22:12 -0700

    Fixed multiple pages. Modified the [panel-content.js] to be more dynamic.

--- js/panel-content.js
+++ js/panel-content.js
@@ -1,21 +1,31 @@
 function displayArticle(rdbutton) {
-    const rdbtn_ids = ["rd-btn-0_to_4_weeks", "rd-btn-4_weeks_to_yr", "rd-btn-yr_to_seven_yrs", "rd-btn-seven_yrs_and_more"];
-    const article_id = ["0_to_4_weeks_article", "4_weeks_to_yr_article", "yr_to_seven_yrs_article", "seven_yrs_and_more_article"];
-    const articles = document.querySelectorAll('div.panel > article');
-
+    const sections = document.querySelectorAll('div.panel section');    // get the sections for each paragraphs of the div.panel (NodeList)
+    const btnGroup = document.querySelectorAll('div.panel div.btn-group'); // get the div.btn-group element (NodeList)
+    // get all the inputs button of type radion in the div.btn-group
+    const rdbtn_inputs = [...btnGroup].map(input => [...input.children].filter(child => {
+        if(child.tagName == "input".toUpperCase()) {
+            return child;
+        }
+        }));
+    const rdbtn_ids = [...rdbtn_inputs[0]].map(rdbtn => rdbtn.id); // make a copy of all the radio buttons id
+    const section_ids = [...sections].map(section => section.id) // make a copy of all the section ids that identify that unique section
 
     let activeIdx = 0;
     rdbtn_ids.forEach((btn_id, idx) => {
-        if (rdbutton.id == btn_id) {
+        // find the target id match and set the appropiate index found later to be use for the sections
+        if (btn_id === rdbutton.id) {
             activeIdx =  idx;
         }
     });
+    
+    for (let idx = 0; idx < sections.length; ++idx) {
+        console.log(`${sections[idx].id} === ${section_ids[activeIdx]}`);
 
-    articles.forEach((article, idx) => {
-        if(idx == activeIdx) {
-            article.style.display = "block";
+        // compare the active index section with to hide or show the content of the session.
+        if(sections[idx].id == section_ids[activeIdx]) {
+            sections[idx].style.display = "block";
         } else {
-            article.style.display = "none";
+            sections[idx].style.display = "none";
         }
-    });
+    }
 }
